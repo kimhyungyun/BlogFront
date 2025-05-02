7c9d3e2211039eb4b9cb765302d3b3cd86717497
somi9954 <jolh999@gmail.com>
2025-05-02 15:28:45 +0900

    images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,14 +7,18 @@ const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 export const metadata: Metadata = {
   title: "HG - Portfolio",
   description: "풀스택 개발자 HG의 포트폴리오입니다.",
+  authors: [{ name: "Hyun_Gyun" }],
   openGraph: {
     title: "BlogFront",
     description: "풀스택 개발자 HG의 포트폴리오입니다",
     url: "https://blog-front-orpin.vercel.app",
     siteName: "BlogFront",
+    type: "website",
+    locale: "ko_KR",
     images: ["/Thumbnail.png"],
   },
 };
+
 export default function RootLayout({
   children,
 }: {
