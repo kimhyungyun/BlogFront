7b8e19a893514459d48edbf84c6a77e64e315938
somi9954 <jolh999@gmail.com>
2025-05-02 15:24:51 +0900

    images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,19 +12,9 @@ export const metadata: Metadata = {
     description: "풀스택 개발자 HG의 포트폴리오입니다",
     url: "https://blog-front-orpin.vercel.app",
     siteName: "BlogFront",
-    images: [
-      {
-        url: "/Thumbnail.png",
-        width: 1200,
-        height: 630,
-        alt: "썸네일",
-      },
-    ],
-    locale: "ko_KR",
-    type: "website",
+    images: ["/Thumbnail.png"],
   },
 };
-
 export default function RootLayout({
   children,
 }: {
